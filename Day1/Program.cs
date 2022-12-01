326813a61f745d8385933fcfab2a61a73b00d6ed
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-12-01 08:54:22 +0000

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<string> input = File.ReadAllLines("InputFile.txt").ToList();
+
+List<Elf> elves = new List<Elf>();
+Elf currentElf = new Elf();
+foreach (string line in input)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        elves.Add(currentElf);
+        currentElf = new Elf();
+    }
+    else
+    {
+        currentElf.Calories.Add(int.Parse(line));
+    }
+}
+
+var topElf = elves.OrderByDescending(e => e.TotalCalories()).First();
+
+Console.WriteLine(topElf.TotalCalories());
+
+var topThree = elves.OrderByDescending(e => e.TotalCalories()).Take((3));
+
+Console.WriteLine(topThree.Sum(e => e.TotalCalories()));
+
+Console.ReadLine();
+
+public class Elf
+{
+    public Elf()
+    {
+        Calories = new List<int>();
+    }
+    public List<int> Calories { get; set; }
+
+    public int TotalCalories()
+    {
+        return Calories.Sum();
+    }
+
+}
