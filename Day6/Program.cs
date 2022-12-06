b401f17a7eded1ec8c3ca510b83ad2e603b25fde
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-12-06 09:19:26 +0000

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using AdventUtilities;
+
+List<string> input = File.ReadAllLines("InputFile.txt").ToList();
+
+string line = input.FirstOrDefault();
+
+int startOfPacket = 0;
+for (int i = 0; i < line.Length; i++)
+{
+    List<char> letters = line.Skip(i).Take(4).ToList();
+
+    var groups = letters.GroupBy(l => l, (letter, count) => new { letter = letter, count = count.Count() });
+
+    if (!groups.Any(g => g.count > 1))
+    {
+        startOfPacket = i+4;
+        break;
+    }
+}
+
+int startOfMessage = 0;
+for (int i = 0; i < line.Length; i++)
+{
+    List<char> letters = line.Skip(i).Take(14).ToList();
+
+    var groups = letters.GroupBy(l => l, (letter, count) => new { letter = letter, count = count.Count() });
+
+    if (!groups.Any(g => g.count > 1))
+    {
+        startOfMessage = i + 14;
+        break;
+    }
+}
+
+Console.WriteLine(startOfPacket);
+Console.WriteLine(startOfMessage);
+Console.ReadLine();
