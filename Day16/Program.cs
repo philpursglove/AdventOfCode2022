2c67f98b030b94866bb49cca4e6c6bf3896a4489
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-12-19 15:56:26 +0000

    Day 19 setup

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,77 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-List<Valve> valves = new List<Valve>();
-
 List<string> input = new List<string>
 {
-	"Valve AA has flow rate=0; tunnels lead to valves DD, II, BB",
-	"Valve BB has flow rate=13; tunnels lead to valves CC, AA",
-	"Valve CC has flow rate=2; tunnels lead to valves DD, BB",
-	"Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE",
-	"Valve EE has flow rate=3; tunnels lead to valves FF, DD",
-	"Valve FF has flow rate=0; tunnels lead to valves EE, GG",
-	"Valve GG has flow rate=0; tunnels lead to valves FF, HH",
-	"Valve HH has flow rate=22; tunnel leads to valve GG",
-	"Valve II has flow rate=0; tunnels lead to valves AA, JJ",
-	"Valve JJ has flow rate=21; tunnel leads to valve II"
+    "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB",
+    "Valve BB has flow rate=13; tunnels lead to valves CC, AA",
+    "Valve CC has flow rate=2; tunnels lead to valves DD, BB",
+    "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE",
+    "Valve EE has flow rate=3; tunnels lead to valves FF, DD",
+    "Valve FF has flow rate=0; tunnels lead to valves EE, GG",
+    "Valve GG has flow rate=0; tunnels lead to valves FF, HH",
+    "Valve HH has flow rate=22; tunnel leads to valve GG",
+    "Valve II has flow rate=0; tunnels lead to valves AA, JJ",
+    "Valve JJ has flow rate=21; tunnel leads to valve II"
 };
-
-Valve currentValve;
-foreach (string inputLine in input)
-{
-	string[] halves = inputLine.Split(";");
-	string[] firstHalf = halves[0].Split(" ");
-
-	currentValve = valves.FirstOrDefault(v => v.Name == firstHalf[1]);
-	if (currentValve == null)
-	{
-		currentValve = new Valve();
-	}
-
-	currentValve.Name = firstHalf[1];
-	currentValve.FlowRate = int.Parse(firstHalf[4].Replace("rate=", string.Empty));
-	string secondHalf = halves[1];
-	secondHalf = secondHalf.Replace(" tunnels lead to valves", string.Empty)
-		.Replace(" tunnel leads to valve", string.Empty).Replace(" ", string.Empty);
-	string[] linkedValves = secondHalf.Split(",");
-	foreach (string valveName in linkedValves)
-	{
-		Valve linkedValve = valves.FirstOrDefault(v => v.Name == valveName);
-		if (linkedValve == null)
-		{
-			linkedValve = new Valve() {Name = valveName};
-			valves.Add(linkedValve);
-		}
-		currentValve.LinkedValves.Add(linkedValve);
-	}
-
-	if (!valves.Contains(currentValve))
-	{
-		valves.Add(currentValve);
-	}
-}
-
-foreach (Valve valve in valves.OrderBy(v => v.Name))
-{
-	Console.WriteLine(valve.Name);
-}
-
-
-
-
-Console.ReadLine();
 
 
 
 public class Valve
 {
-	public Valve()
-	{
-		LinkedValves = new List<Valve>();
-	}
-	public string Name { get; set; }
-	public int FlowRate { get; set; }
-	public bool Open { get; set; }
+    public Valve(string name, int flowRate)
+    {
+        Name = name;
+        FlowRate = flowRate;
+    }
 
-	public List<Valve> LinkedValves { get; set; }
+    public string Name { get; set; }
+    public int FlowRate { get; set; }
+    public bool Open { get; set; }
+
 }
---
+++ Day19/Program.cs
@@ -0,0 +1,86 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<string> input = new List<string>
+{
+    "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian."
+};
+
+
+
+int oreBots = 1;
+int clayBots = 0;
+int ore = 0;
+int clay = 0;
+bool clayBuilding = false;
+bool obsidianBuilding = false;
+int obsidianBots = 0;
+int obsidian = 0;
+bool geodeBuilding = false;
+int geode = 0;
+int geodeBots = 0;
+
+for (int minute = 1; minute < 25; minute++)
+{
+    if (ore >= 2)
+    {
+        if (obsidian >= 7)
+        {
+            geodeBuilding = true;
+            ore -= 2;
+            obsidian -= 7;
+        }
+        else
+        {
+            if (clay >= 7)
+            {
+                if (ore >= 3 && clay >= 14 && geodeBots == 0)
+                {
+                    obsidianBuilding = true;
+                    ore -= 3;
+                    clay -= 14;
+                }
+            }
+            else
+            {
+                clayBuilding = true;
+                ore -= 2;
+            }
+        }
+    }
+
+    ore += oreBots;
+    clay += clayBots;
+    obsidian += obsidianBots;
+    geode += geodeBots;
+
+    if (clayBuilding)
+    {
+        clayBots++;
+        clayBuilding = false;
+    }
+
+    if (obsidianBuilding)
+    {
+        obsidianBots++;
+        obsidianBuilding = false;
+    }
+
+    if (geodeBuilding)
+    {
+        geodeBots++;
+        geodeBuilding = false;
+    }
+}
+
+
+Console.WriteLine($"OreBots: {oreBots}");
+Console.WriteLine($"ClayBots: {clayBots}");
+Console.WriteLine($"ObsidianBots: {obsidianBots}");
+Console.WriteLine($"GeodeBots: {geodeBots}");
+
+Console.WriteLine($"Ore: {ore}");
+Console.WriteLine($"Clay: {clay}");
+Console.WriteLine($"Obsidian: {obsidian}");
+Console.Write($"Geodes: {geode}");
+
+Console.ReadLine();
